10e5319835f4b06cee454c6233a441c2bea27a80
rdab <rdamian.alfonso@riseup.net>
2019-06-04 16:46:20 -0400

    import game module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {GameModule } from './game/game.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GameModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
