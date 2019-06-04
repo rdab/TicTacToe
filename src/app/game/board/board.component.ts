8f7e6808693e562917c1b966d2bcc75d01e7ca70
rdab <rdamian.alfonso@riseup.net>
2019-06-04 18:14:52 -0400

    Bootstrap the 9 board squares

--- src/app/game/board/board.component.ts
+++ src/app/game/board/board.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
+  private _values: string[][] = [
+    ['-','-','-',],
+    ['-','-','-',],
+    ['-','-','-',],
+  ];
+
   constructor() { }
 
   ngOnInit() {
