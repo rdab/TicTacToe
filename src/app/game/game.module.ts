4ec3cd80b21bf37511c82bdfbc3bdb9fe9463bd5
rdab <rdamian.alfonso@riseup.net>
2019-06-04 17:39:53 -0400

    Add new component: square

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { GameComponent } from './game/game.component';
 import { HeaderComponent } from './header/header.component';
 import { BoardComponent } from './board/board.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
-  declarations: [GameComponent, HeaderComponent, BoardComponent],
+  declarations: [GameComponent, HeaderComponent, BoardComponent, SquareComponent],
   imports: [
     CommonModule
   ],
