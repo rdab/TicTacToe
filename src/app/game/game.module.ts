3ed7e4ba1514a554115e7327891c38eee5d83354
rdab <rdamian.alfonso@riseup.net>
2019-06-04 17:28:41 -0400

    Add header and board components

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameComponent } from './game/game.component';
+import { HeaderComponent } from './header/header.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
-  declarations: [GameComponent],
+  declarations: [GameComponent, HeaderComponent, BoardComponent],
   imports: [
     CommonModule
   ],
