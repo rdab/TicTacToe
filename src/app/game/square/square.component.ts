8270d5b2b156b1ea80558f3045858fb45c2780b7
rdab <rdamian.alfonso@riseup.net>
2019-06-04 17:53:16 -0400

    annotate input fields in square components

--- src/app/game/square/square.component.ts
+++ src/app/game/square/square.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SquareComponent implements OnInit {
 
-  row: number;
-  column: number;
+  @Input() row: number;
+  @Input() column: number;
 
   constructor() { }
 
