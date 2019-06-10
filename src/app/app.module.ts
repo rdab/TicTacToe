25956b111dac48379ef9b65a81bbf1fabb45c46f
rdab <rdamian.alfonso@riseup.net>
2019-06-10 19:20:46 -0400

    Import http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {GameModule } from './game/game.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { IndexComponent } from './index/index.component';
     BrowserModule,
     GameModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
