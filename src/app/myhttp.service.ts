5abf9fdf95a864fbc997a350fe2b3ded47d9255f
rdab <rdamian.alfonso@riseup.net>
2019-06-10 19:27:58 -0400

    Initialize service class

--- src/app/myhttp.service.ts
+++ src/app/myhttp.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
 export class MyhttpService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getSavedGame(){
+    return this.httpClient.get('https://api.myjson.com/bins/i216a');
+  }
 }
