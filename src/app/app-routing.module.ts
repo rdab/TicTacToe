27f66f2c98e609eac93061bf60beb209d08a5ecf
rdab <rdamian.alfonso@riseup.net>
2019-06-10 17:26:37 -0400

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { GameComponent } from './game/game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent
+  },
+  {
+    path: 'new',
+    component: GameComponent
+  },
+  {
+    path: 'continue',
+    component: GameComponent,
+    data: { continue: true }
+  },
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
